68084363d4a06cce654284b55d8eee968f219526
behnamparsa <bparsa@Gmail.com>
2025-01-27 08:25:22 -0500

    Jan 27 8:25

---
+++ Practice ML/cl_3d_analysis.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+
+
+def plot(df):
+    fig = plt.figure(figsize = (16,9))
+    ax = fig.add_subplot(projection = '3d')
+    ax.scatter(df['income'],df['spending'],df['age'],c = df['cluster'], cmap = 'plasma')
+    ax.set_xlabel('Income')
+    ax.set_ylabel('Spending')
+    ax.set_zlabel('Age')
+
+    df.drop('gender', axis=1, inplace=True)
+    means = df.groupby(by='cluster').mean()
+
+
+    ax.scatter(means['income'],means['spending'],means['age'], color ='red', s = 100)
+
+    plt.show()
+
+def plot_elbow(df,max):
+    plot = {}
+
+    for n in range(2,max + 1):
+        inertia = cluster(df, n)
+        plot[n] = inertia
+
+    fig = plt.figure(figsize = (16,9))
+    ax = fig.add_subplot()
+    ax.set_xlabel("Number of Clusters")
+    ax.set_ylabel("Inertia")
+    ax.plot(plot.keys(),plot.values())
+    plt.show()
+
+
+
+
+def cluster(df,n):
+    X = df[['income','spending','age']]
+    X = StandardScaler().fit_transform(X)
+
+    model = KMeans(n_clusters= n)
+    model.fit(X)
+    df['cluster'] = model.labels_
+    return model.inertia_
+
+
+def main():
+    
+    df = pd.read_csv('mall_customers.csv')
+    df.columns = ['id','gender','age','income','spending']
+
+    cluster(df,6)
+    plot_elbow(df,10)
+#    plot(df)
+
+
+   
+
+
+main()
---
+++ Practice ML/cl_iris.py
@@ -0,0 +1,17 @@
+import sklearn.datasets as ds
+
+
+data = ds.load_iris(as_frame = True)
+
+print(data.keys())
+
+target = data['target']
+
+target_names= data['target_names']
+
+df = data['data']
+df['species_index'] = target
+df['species'] = target.map(lambda n: target_names[n])
+
+
+print(df)
